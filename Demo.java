0c37154529acfa3eb4f971a4e599a7c95a32c640
AustinSweet <61333860+AustinSweet@users.noreply.github.com>
2021-06-02 20:41:28 -0500

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -3,7 +3,8 @@ public class Demo {
     public static void main(String[] xxx) {
         int x = 1;
         int y = 2;
-        System.out.println(x + " " + y);
+        int z = 3;
+        System.out.println(x + " " + y + " " + z);
     }
 
 }
